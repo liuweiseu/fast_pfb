23c0acbb80b02ba89b62489da91e7b0ef3f9361b
Wei Liu <liuwei_berkeley@berkeley.edu>
2022-01-14 22:54:55 -0800

    modify cuda_pfb.py, making it work with C code.

--- cuda_pfb.py
+++ cuda_pfb.py
@@ -3,7 +3,7 @@ import numpy as np
 from katsdpsigproc import accel
 from katgpucbf.fgpu import compute
 
-def cuda_pfb(h_in, weights):
+def cuda_pfb(h_in):
 
     spectra_per_heap = 128
     taps = 4
@@ -13,6 +13,12 @@ def cuda_pfb(h_in, weights):
     in_offset = 0
     out_offset = 0
     
+    weights = np.ones((2 * channels * taps,)).astype(np.float32)
+    try:
+        h_in = np.frombuffer(h_in, dtype=np.uint8)
+    except:
+        pass
+
     ctx = accel.create_some_context()
     queue = ctx.create_command_queue()
     template = compute.ComputeTemplate(ctx, taps)
@@ -36,8 +42,6 @@ def cuda_pfb(h_in, weights):
     fn.pfb_fir[0].spectra = spectra
     fn.pfb_fir[0]()
     '''
-  
-
     fn.ensure_all_bound()
     fn.fft[0]()
     #fn.fft[1]()
@@ -45,5 +49,16 @@ def cuda_pfb(h_in, weights):
     #fft_out1 = fn.buffer('fft_out1').get(queue)
     fft_out0[:,0] = 0
     fft_out0_flat = fft_out0.flatten()
-    return fft_out0_flat
+    #return abs(fft_out0_flat).tolist()
+    return abs(fft_out0_flat)
+
+def test(d):
+    print("Hello!")
+    d = np.frombuffer(d, dtype=np.uint8)
+    print(type(d))
+    print(d.shape)
+    print(d)
+    res = np.ndarray((10,)).astype(np.float32)
+    print(res)
+    return res.tolist()
     
