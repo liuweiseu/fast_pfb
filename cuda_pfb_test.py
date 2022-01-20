8d2d6e2682e26ee2ca79ac6dd620095714e322ce
Wei Liu <liuwei_berkeley@berkeley.edu>
2022-01-19 16:52:04 -0800

    modify cuda_pfb_test.py.

--- cuda_pfb_test.py
+++ cuda_pfb_test.py
@@ -32,22 +32,14 @@ def cuda_pfb(h_in):
     # set data and weights to 'in' and 'weights' buffer
     fn.buffer('in0').set(queue,h_in)
     fn.pfb_fir[0].buffer('weights').set(queue,weights)
-    #fn.buffer('in1').set(queue,h_in)
-    #fn.pfb_fir[1].buffer('weights').set(queue,weights)
-    
+    fn.ensure_all_bound()
+
     t2 = time.time()
 
-    fn.ensure_all_bound()
     fn.pfb_fir[0].in_offset = in_offset
     fn.pfb_fir[0].out_offset = out_offset
     fn.pfb_fir[0].spectra = spectra
     fn.pfb_fir[0]()
-    '''
-    fn.pfb_fir[0].in_offset = in_offset
-    fn.pfb_fir[0].out_offset = out_offset
-    fn.pfb_fir[0].spectra = spectra
-    fn.pfb_fir[0]()
-    '''
 
     t3 = time.time()
 
@@ -55,19 +47,17 @@ def cuda_pfb(h_in):
     fn.fft[0]()
 
     t4 = time.time()
-    #fn.fft[1]()
+
     fft_out0 = fn.buffer('fft_out0').get(queue)
 
     t5 = time.time()
 
-    print("Time for creating context: ", (t1-t0)*1000, "ms")
-    print("Time for setting data: ", (t2-t1)*1000, "ms")
-    print("Time for pfb fir: ", (t3-t2)*1000, "ms")
-    print("Time for pfb fft: ", (t4-t3)*1000, "ms")
-    print("Time for copyting result out: ", (t5-t4)*1000, "ms")
+    print("%-35s: %f ms"%("   Time for creating context", (t1-t0)*1000))
+    print("%-35s: %f ms"%("   Time for setting data", (t2-t1)*1000))
+    print("%-35s: %f ms"%("   Time for pfb fir", (t3-t2)*1000))
+    print("%-35s: %f ms"%("   Time for pfb fft", (t4-t3)*1000))
+    print("%-35s: %f ms"%("   Time for copyting result out", (t5-t4)*1000))
 
-    #fft_out1 = fn.buffer('fft_out1').get(queue)
     fft_out0[:,0] = 0
     fft_out0_flat = fft_out0.flatten()
-    #return abs(fft_out0_flat).tolist()
     return abs(fft_out0_flat)
